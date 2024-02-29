9d407fe8cbbf7e04c2d83af2b159e51073fb8823
Trey Hardin <treyhardindesign@gmail.com>
2024-02-29 10:16:18 -0500

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from '@astrojs/sitemap';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), sitemap()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  site: 'https://meetdomaine.com/',
 });
