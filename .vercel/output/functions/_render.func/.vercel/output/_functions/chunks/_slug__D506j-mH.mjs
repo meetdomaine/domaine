f7d5d92c60a1c2399162fd04f6e7e2938a56fa5c
Trey Hardin <treyhardindesign@gmail.com>
2024-02-29 14:35:30 -0500

    Responsiveness fixes

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_slug__D506j-mH.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_CC4kZmfH.mjs').then(n => n.d);
-
-export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/robots_CGsqGSBD.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_DLY4VI4v.mjs').then(n => n.r);
+
+export { page };
