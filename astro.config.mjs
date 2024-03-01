b754cc8e5e7fd33876d434d9d6d9c0bb21984b7c
Trey Hardin <treyhardindesign@gmail.com>
2024-03-01 12:04:45 -0500

    Switch adapter and remove prerender from About section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-// import vercel from "@astrojs/vercel/serverless";
-import vercelStatic from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/serverless";
+// import vercelStatic from '@astrojs/vercel/static';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), sitemap()],
   output: 'hybrid',
-  adapter: vercelStatic(),
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
   // adapter: vercel(),
   site: 'https://meetdomaine.com/',
 });
