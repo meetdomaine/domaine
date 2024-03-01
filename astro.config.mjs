bf9961e02e764ab891d04aee240b9041571e0001
Trey Hardin <treyhardindesign@gmail.com>
2024-03-01 11:32:19 -0500

    Change vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), sitemap()],
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: vercelStatic(),
+  // adapter: vercel(),
   site: 'https://meetdomaine.com/',
 });
