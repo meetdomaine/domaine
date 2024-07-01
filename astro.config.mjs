9d9676c58a3bdde882cd66595b17484fbeeab7c9
Trey Hardin <trey@meetdomaine.com>
2024-07-01 09:22:41 -0400

    Remove old deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,28 +4,17 @@ import vercel from "@astrojs/vercel/serverless";
 // import vercelStatic from '@astrojs/vercel/static';
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
-import sanity from "@sanity/astro";
-import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sitemap(), 
-    icon(), 
-    sanity({
-      projectId: 'cxeknc6v',
-      dataset: 'production',
-      useCdn: false,
-      studioBasePath: '/admin',
-    }), 
+    icon(),
     solid({ 
       devtools: true ,
       include: 'src/**/*'
     }),
-    react({
-      include: 'domaine-cms/**/*'
-    }), 
   ],
   prefetch: true,
   output: 'hybrid',
