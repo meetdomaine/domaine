3eca59549d066033b2707226f287bde54da6419b
Trey Hardin <trey@meetdomaine.com>
2024-07-22 13:07:15 -0400

    Parallax

--- src/components/global/utils/gsap.js
+++ src/components/global/utils/gsap.js
@@ -1,10 +1,23 @@
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(TextPlugin, SplitText);
+gsap.registerPlugin(TextPlugin, SplitText, ScrollTrigger);
 
-export { gsap, TextPlugin, SplitText }
+export { gsap, TextPlugin, SplitText, ScrollTrigger }
+
+export const addParallax = (element, distance = "15%") => {
+    gsap.to(element, {
+        scrollTrigger: {
+            trigger: element,
+            start: 'top top',
+            end: 'bottom top',
+            scrub: true,
+        },
+        y: distance,
+    });
+}
 
 export const fadeUp = (element, timeline, delay, stagger) => {
     timeline.from(element, {
