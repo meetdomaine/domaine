3cdc1985f17ab2368f8f84a1f825131b22e14e31
Trey Hardin <trey@meetdomaine.com>
2024-08-02 21:29:47 -0400

    Animations

--- src/components/global/utils/gsap.js
+++ src/components/global/utils/gsap.js
@@ -2,17 +2,18 @@ import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 import { SplitText } from "gsap/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { CustomEase } from "gsap/all";
 
-gsap.registerPlugin(TextPlugin, SplitText, ScrollTrigger);
+gsap.registerPlugin(TextPlugin, SplitText, ScrollTrigger, CustomEase);
 
 export { gsap, TextPlugin, SplitText, ScrollTrigger }
 
-export const addParallax = (element, distance = "12%") => {
-    gsap.to(element, {scale: 1.185, duration: 0})
+export const addParallax = (element, distance = "30%") => {
+    gsap.to(element, {scale: 1.1, duration: 0})
     gsap.to(element, {
         scrollTrigger: {
             trigger: element,
-            start: 'top bottom',
+            start: 'top middle',
             end: 'bottom top',
             scrub: true,
         },
@@ -28,3 +29,16 @@ export const fadeUp = (element, timeline, delay, stagger) => {
         stagger: stagger
     }, delay)
 }
+
+export const zoomIn = (element, timeline, delay, stagger) => {
+    timeline.from(element, {
+        scrollTrigger: element,
+        opacity: 0,
+        scale: 1.2,
+        // autoAlpha: 0,
+        // stagger: stagger
+    }, delay)
+}
+
+CustomEase.create( "cubic", "M0,0 C0.22,1 0.36,1 1,1 ")    
+CustomEase.create("expo", "M0,0 C0.16,1 0.3,1 1,1 ");
