1eb4075b55d67990ae7de1a719c6787e3306354d
Trey Hardin <trey@meetdomaine.com>
2024-09-24 11:41:48 -0400

    Service type hero mobile

--- src/components/global/utils/check-device.js
+++ src/components/global/utils/check-device.js
@@ -1,1 +1,7 @@
-export const isMobileBreakpoint = window.innerWidth <= 768
+export let isMobileBreakpoint = window.innerWidth <= 768
+
+export const getIsMobile = () => {
+  const isMobileBrowser = navigator.userAgentData.mobile
+  const isMobileBreakpoint = window.innerWidth <= 768
+  return (isMobileBrowser || isMobileBreakpoint)
+}
