29e0f01ecb0fb2f8b54eae309443a1d1d17c366f
Trey Hardin <trey@meetdomaine.com>
2025-05-27 12:03:29 -0400

    Try rewrite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,5 +57,12 @@ export default defineConfig({
       "import.meta.env.HUBSPOT_ACCESS_TOKEN": JSON.stringify(process.env.HUBSPOT_ACCESS_TOKEN),
       "import.meta.env.GREENSOCK_AUTH_TOKEN": JSON.stringify(process.env.GREENSOCK_AUTH_TOKEN),
     },
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   },
 });
