a9b061aa192914100753e448c7836fcdf29d0afa
Trey Hardin <trey@meetdomaine.com>
2025-08-13 15:57:31 -0400

    Test different fetch strategies

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,10 +20,6 @@ const PUBLIC_SANITY_API_READ_TOKEN = process.env.PUBLIC_SANITY_API_READ_TOKEN ||
 const PUBLIC_SANITY_VISUAL_EDITING_ENABLED = process.env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED || env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED;
 // console.log(SANITY_API_READ_TOKEN)
 
-// Debug environment variables
-console.log('SANITY_API_READ_TOKEN available:', !!PUBLIC_SANITY_API_READ_TOKEN)
-console.log('PUBLIC_SANITY_VISUAL_EDITING_ENABLED available:', !!PUBLIC_SANITY_VISUAL_EDITING_ENABLED)
-
 export default defineConfig({
   integrations: [
     sitemap(), 
