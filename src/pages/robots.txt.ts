fa8df7934525fda73fa81b41fb548124383ae9bb
Grady Woodruff <grady.woodruff@meetdomaine.com>
2025-10-22 19:50:41 -0500

    [DS-108] Update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,9 +2,20 @@ import type { APIRoute } from 'astro';
 
 const robotsTxt = `
 User-agent: *
-Allow: /
 
-Sitemap: ${new URL('sitemap-index.xml', 'https://meetdomaine.com/').href}
+Disallow: /admin/*
+Disallow: /404
+Disallow: /_astro/*
+Disallow: /cdn/*
+Disallow: /services/undefined/*
+
+User-agent: Bingbot
+Disallow: /llms.txt
+
+User-agent: Googlebot
+Disallow: /llms.txt
+
+Sitemap: https://meetdomaine.com/sitemap.xml
 `.trim();
 
 export const GET: APIRoute = () => {
